1f7b9a72011437c6fb6366dbd49bae10767c98f5
ASUS <ASUS@DESKTOP-OTG3GSS>
2018-09-22 19:25:29 +0530

    changed

--- src/main/java/com/app/OrmEx.java
+++ src/main/java/com/app/OrmEx.java
@@ -3,6 +3,8 @@ package com.app;
 public class OrmEx {
 	public static void main(String[] args) {
 		System.out.println("This is from Testxxx");
+		System.out.println("This is changed line");
+		
 	}
 
 }
