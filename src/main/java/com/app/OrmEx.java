ed7d7dfd7cb951e4fb0b71efe55890b4686f0b7f
surendra55 <43514268+surendra55@users.noreply.github.com>
2018-09-23 17:12:55 +0530

    Update OrmEx.java

--- src/main/java/com/app/OrmEx.java
+++ src/main/java/com/app/OrmEx.java
@@ -4,6 +4,8 @@ public class OrmEx {
 	public static void main(String[] args) {
 		System.out.println("This is from Testxxx");
 		System.out.println("This is changed line");
+		System.out.println("HELLO WORLD2");
+	}
 		
 	}
 
